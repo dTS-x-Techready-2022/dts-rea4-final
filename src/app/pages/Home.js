db0d9f79caa6f8ae812e0e2ff3cd48a8321c41e0
rendyproklamanta <rendydwi@myself.com>
2022-10-19 15:24:10 +0700

    First

---
+++ src/app/pages/Home.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+import UserService from "../services/express/user.service";
+
+const Home = () => {
+   const [content, setContent] = useState("");
+
+   useEffect(() => {
+      UserService.getPublicContent().then(
+         (response) => {
+            console.log(response);
+            setContent(response.data);
+         },
+         (error) => {
+            console.log(error);
+
+            const _content =
+               (error.response && error.response.data) ||
+               error.message ||
+               error.toString();
+
+            setContent(_content);
+         }
+      );
+   }, []);
+
+   return (
+      <div className="container">
+         <header className="jumbotron">
+            <h3>{content}</h3>
+         </header>
+      </div>
+   );
+};
+
+export default Home;
---
+++ src/app/utils/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/analytics';
+
+const firebaseConfig = {
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+firebase.auth();
+firebase.analytics();
+firebase.storage();
+
+export default firebase;
+export const firestore = firebase.firestore();
