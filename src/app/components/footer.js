e75b4ddffe744ae62b04d67afdfd9e6c35f6068a
temaanggaraunsia <temaanggara02@student.unsia.ac.id.com>
2022-10-22 15:42:08 +0800

    footer.js

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -7,7 +7,10 @@ function Footer() {
       <>
          <Container fluid className='bg-light'>
             <div class="container-fluid py-4 px-sm-3 px-md-5">
-                 <p class="m-0 text-center">© <a class="font-weight-bold" href="https://www.free-css.com/free-css-templates">Your Site Name</a>. All Rights Reserved. Designed by <a class="font-weight-bold" target="_blank" rel="nofollow noopener" href="//htmlcodex.com">HTML Codex</a></p>
+                 <p class="m-0 text-center">
+                    <a style={{ fontWeight: 800 }}><span style={{ color: 'red' }}>INDO</span><span className="text-black">NEWS </span></a> 
+                    All Rights Reserved. Designed by <a class="font-weight-bold" target="_blank" rel="nofollow noopener" href="//htmlcodex.com">HTML Codex</a>
+                </p>
             </div>
          </Container>
       </>
