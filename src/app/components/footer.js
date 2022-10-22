30eeff575f83ad1b6a534cdeb1583950f934e5b8
temaanggaraunsia <temaanggara02@student.unsia.ac.id.com>
2022-10-22 15:14:05 +0800

    footer.js

---
+++ src/app/components/footer.js
@@ -0,0 +1,17 @@
+import Container from 'react-bootstrap/Container';
+
+function Footer() {
+
+
+   return (
+      <>
+         <Container fluid className='bg-light'>
+            <div class="container-fluid py-4 px-sm-3 px-md-5">
+                 <p class="m-0 text-center">© <a class="font-weight-bold" href="https://www.free-css.com/free-css-templates">Your Site Name</a>. All Rights Reserved. Designed by <a class="font-weight-bold" target="_blank" rel="nofollow noopener" href="//htmlcodex.com">HTML Codex</a></p>
+            </div>
+         </Container>
+      </>
+   );
+}   
+
+export default Footer;
