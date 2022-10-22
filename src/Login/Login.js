e80f2ca4f164d76f4ab1122753b5b5f9d569c3e1
royanoz <113403867+royanoz@users.noreply.github.com>
2022-10-22 10:35:17 +0700

    Create Login.js
    Sebelum ditambahkan css / UI

---
+++ src/Login/Login.js
@@ -0,0 +1,67 @@
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+//import './forms.css'
+import {signInWithEmailAndPassword, sendEmailVerification} from 'firebase/auth'
+import {auth} from '../Config/firebase'
+import {useNavigate} from 'react-router-dom'
+import {useAuthValue} from './AuthContext'
+
+
+function Login(){
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('') 
+  const [error, setError] = useState('')
+  const {setTimeActive} = useAuthValue()
+  const navigate = useNavigate()
+
+  const login = e => {
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      if(!auth.currentUser.emailVerified) {
+        sendEmailVerification(auth.currentUser)
+        .then(() => {
+          setTimeActive(true)
+          navigate('/verify-email')
+        })
+      .catch(err => alert(err.message))
+    }else{
+      navigate('/')
+    }
+    })
+    .catch(err => setError(err.message))
+  }
+
+  return(
+    <div className='center'>
+      <div className='auth'>
+        <h1>Log in</h1>
+        {error && <div className='auth__error'>{error}</div>}
+        <form onSubmit={login} name='login_form'>
+          <input 
+            type='email' 
+            value={email}
+            required
+            placeholder="Masukan Email"
+            onChange={e => setEmail(e.target.value)}/>
+
+          <input 
+            type='password'
+            value={password}
+            required
+            placeholder='Masukan Password'
+            onChange={e => setPassword(e.target.value)}/>
+
+          <button type='submit'>Login</button>
+        </form>
+        <p>
+          Belum Punya Akun? 
+          <Link to='/register'>Daftar Disini</Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Login
