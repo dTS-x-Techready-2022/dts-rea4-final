ebb5030c0cbe67ec21a74f7bd03e70b5a9482bc8
Feriy wahyu <feriywahyu@gmail.com>
2022-10-23 11:35:36 +0800

    del

--- src/utils/firebase/listener.js
+++
@@ -1,6 +0,0 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './base';
-
-export const listener = (cb) => {
-    onAuthStateChanged(auth, (user) => cb(user ? user.uid : false));
-};
