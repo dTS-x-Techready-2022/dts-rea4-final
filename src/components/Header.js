599360e9115a16bd5b06efea736296715b0132f7
syaifudin <sfnalkatiri@gmail.com>
2022-10-26 11:25:46 +0700

    dev alka fetch api and render api

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,29 +6,50 @@ import SearchIcon from "@mui/icons-material/Search";
 import InputBase from "@mui/material/InputBase";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+import News from "./News";
+
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <Container maxWidth="xl">
-      <div className="header-wrap">
-        <img src={logo} alt="Logo" height={50} />
-        <div className="header-menu-wrap">
-          <InputBase
-            placeholder="Search…"
-            inputProps={{ "aria-label": "search" }}
-          />
-          <SearchIcon />
-          <MenuIcon />
-        </div>
-      </div>
-      <ul className="menu" >
-        <li>News</li>
-        <li>Sport</li>
-        <li>Sains</li>
-        <li>Tech</li>
-        <li>Entertainment</li>
-        <li>Life Style</li>
-      </ul>
-    </Container>
-  );
+	return (
+		<Container maxWidth="xl">
+			<div className="header-wrap">
+				<img src={logo} alt="Logo" height={50} />
+				<div className="header-menu-wrap">
+					<InputBase
+						placeholder="Search…"
+						inputProps={{ "aria-label": "search" }}
+					/>
+					<SearchIcon />
+					<MenuIcon />
+				</div>
+			</div>
+			<Router>
+				<ul className="menu">
+					<li>
+						<Link to={"/"}>Terbaru</Link>
+					</li>
+					<li>
+						<Link to={"/olahraga"}>Olahraga</Link>
+					</li>
+					<li>
+						<Link to={"/ekonomi"}>Ekonomi</Link>
+					</li>
+					<li>
+						<Link to={"/tekno"}>Teknologi</Link>
+					</li>
+					<li>
+						<Link to={"/hiburan"}>Hiburan</Link>
+					</li>
+					<li>
+						<Link to={"/lifestyle"}>Lifestyle</Link>
+					</li>
+				</ul>
+				<Routes>
+					<Route exact path="/" element={<News />} />
+					<Route exact path="/:category" element={<News />} />
+				</Routes>
+			</Router>
+		</Container>
+	);
 }
